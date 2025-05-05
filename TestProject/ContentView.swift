a2cdf3d28c4e996fd9cd9fd9af77a52faa563813
Tiziano <tiziano.olmetti@gmail.com>
2025-05-06 09:42:16 +1000

    restore

--- TestProject/ContentView.swift
+++ TestProject/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")q
+            Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")

dfdffd7189f2a7ac26d1df7e4ad28848ff7dc57f
Tiziano <tiziano.olmetti@gmail.com>
2025-05-05 23:36:59 +1000

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Tiziano on 5/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
